3c6e05d877bef8f7edd433567d5d0b9eda24e9a2
Roberto Rigo <robertorigo18@gmail.com>
2026-08-13 14:27:46 -0300

    feat: adiciona lado do cliente

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,10 +1,18 @@
 import { Routes } from '@angular/router';
 import { Home } from './presentation/pages/home/home';
+import { ClientSide } from './presentation/sides/client-side/client-side';
 
 export const routes: Routes = [
-    {
+    {//home deslogado
         path: '',
         component: Home,
         title: 'BANCO DA MONSTER HIGH????'
+    },
+    { //todas as telas do lado do cliente
+        path:'client',
+        component:ClientSide,
+        title:'BANCO DA MONSTER HIGH????',
+        children:[]
+
     },
 ];
