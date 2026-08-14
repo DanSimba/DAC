f7951506fced815e20cc37884b94383134f0a622
Vinicius Alves dos Santos <viniciussantos1605@gmail.com>
2026-08-13 22:30:20 -0300

    feat() - Criado models e enums básicos

---
+++ frontend/src/app/models/address.model.ts
@@ -0,0 +1,10 @@
+export interface Address {
+    cep          : string;
+    address      : string;
+    number       : string;
+    complement   : string;
+    neighborhood : string;
+    city         : string;
+    uf           : string;
+    state        : string;
+}
