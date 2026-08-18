be8952aef8b6fb92b6ec3c17ac5c3c6c427fdbb4
GuilhermeND <guilhermegrocha2006@gmail.com>
2026-08-17 21:11:42 -0300

    WIP: ms-cliente

---
+++ backend/ms-cliente/src/main/java/com/monsterbank/ms_cliente/cliente/ClienteRepository.java
@@ -0,0 +1,8 @@
+package com.monsterbank.ms_cliente.cliente;
+
+
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ClienteRepository {
+}
---
+++ backend/ms-cliente/src/main/java/com/monsterbank/ms_cliente/cliente/ClienteService.java
@@ -0,0 +1,24 @@
+package com.monsterbank.ms_cliente.cliente;
+
+
+import org.springframework.stereotype.Service;
+
+import com.monsterbank.ms_cliente.cliente.clienteDTOs.ClienteRetorno;
+
+import java.util.List;
+
+@Service
+public class ClienteService {
+
+
+    public List<ClienteRetorno> listarClientes(Integer page){
+
+
+    }
+
+
+    public List<ClienteRetorno> procurarCliente(String field){
+
+    }
+
+}
