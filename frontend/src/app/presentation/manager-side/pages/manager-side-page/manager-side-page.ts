3d8c35ef79479dcf571f43faafea48c36a1f99c7
DanSimba <danteksimba@gmail.com>
2026-08-21 08:47:16 -0300

    add: componentes para a home do lado gerente

---
+++ frontend/src/app/presentation/manager-side/pages/manager-side-page/manager-side-page.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-manager-side-page',
+  imports: [],
+  templateUrl: './manager-side-page.html',
+  styleUrl: './manager-side-page.css',
+})
+export class ManagerSidePage {
+
+}
