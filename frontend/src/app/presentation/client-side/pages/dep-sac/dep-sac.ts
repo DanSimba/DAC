1a74a04d764b32d34a7badf8d66e4e8b5bb59657
Roberto Rigo <robertorigo18@gmail.com>
2026-08-22 20:57:55 -0300

    feat: faz parte da tela DepSac

--- frontend/src/app/presentation/client-side/pages/dep-sac/dep-sac.ts
+++ frontend/src/app/presentation/client-side/pages/dep-sac/dep-sac.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
 
 @Component({
   selector: 'app-dep-sac',
@@ -6,4 +6,10 @@ import { Component } from '@angular/core';
   templateUrl: './dep-sac.html',
   styleUrl: './dep-sac.css',
 })
-export class DepSac {}
+export class DepSac {
+  side = signal<string>('');
+
+  setSide(s: string){
+    this.side.set(s);
+  }
+}
