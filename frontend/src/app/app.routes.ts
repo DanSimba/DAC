2f3b5522d4eb46a789c2461d2c802ed6b0527d2f
DanSimba <danteksimba@gmail.com>
2026-08-31 15:45:28 -0300

    mod: adiciona rota home gerente

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -2,6 +2,7 @@ import { Routes } from '@angular/router';
 import { HomePage } from './presentation/shared/pages/home-page/home-page';
 import { ClientSidePage } from './presentation/client-side/pages/client-side-page/client-side-page';
 import { Autocadastro } from './presentation/shared/pages/autocadastro/autocadastro';
+import { ManagerSidePage } from './presentation/manager-side/pages/manager-side-page/manager-side-page';
 
 export const routes: Routes = [
     {//home deslogado
@@ -28,6 +29,18 @@ export const routes: Routes = [
                 path:'depsac',
                 loadComponent: () => import('../app/presentation/client-side/pages/dep-sac/dep-sac').then(m => m.DepSac)
             },
+        ]
+    },
+    { //telas do lado gerente
+        path:'manager',
+        component:ManagerSidePage,
+        title:'ah é',
+        children:[
+            {
+                path:'',
+                loadComponent: () => import('../app/presentation/manager-side/pages/manager-home-page/manager-home-page').then(m => m.ManagerHomePage)
+            },
+
         ]
     },
     { // Teste do ViaCEP
