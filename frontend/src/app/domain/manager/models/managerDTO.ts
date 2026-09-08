2ab77782aed2e59f8cad784717d66aea4b0cf310
DanSimba <danteksimba@gmail.com>
2026-09-08 20:58:32 -0300

    add: adiciona services do gerente [calma, já faço rodar o front de novo, me dá 15 minutos eu to com sono]

---
+++ frontend/src/app/domain/manager/models/managerDTO.ts
@@ -0,0 +1,10 @@
+export interface ManagerDTO{
+    // famoso fiquei na dúvida e segui o q tá rolando no back
+    id? : number;
+    nome : string;
+    cpf : string;
+    email : string;
+    telefone : string;
+    
+
+}
