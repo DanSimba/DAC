0a77f801e1e43794a0827ab8f570b6b1ee3978d3
DanSimba <danteksimba@gmail.com>
2026-09-15 14:07:41 -0300

    mod: finalmente a porra dessa navbar dos infernos

--- frontend/src/app/presentation/manager-side/components/manager-nav-bar/manager-nav-bar.ts
+++ frontend/src/app/presentation/manager-side/components/manager-nav-bar/manager-nav-bar.ts
@@ -1,9 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { RouterLink, RouterLinkActive } from '@angular/router';
+import { MatIconModule } from '@angular/material/icon';
+import { ManagerService } from '../../../../application/manager/services/manager-service';
+import { ManagerMenu } from '../manager-menu/manager-menu';
 
 @Component({
   selector: 'app-manager-nav-bar',
-  imports: [],
+  imports: [RouterLink, RouterLinkActive, MatIconModule, ManagerMenu],
   templateUrl: './manager-nav-bar.html',
   styleUrl: './manager-nav-bar.css',
 })
-export class ManagerNavBar {}
+export class ManagerNavBar {
+  managerService = inject(ManagerService);
+}
--- frontend/src/app/presentation/manager-side/pages/manager-side-page/manager-side-page.ts
+++ frontend/src/app/presentation/manager-side/pages/manager-side-page/manager-side-page.ts
@@ -1,20 +1,11 @@
-import { Component, computed, inject } from '@angular/core';
-import { RouterOutlet } from "@angular/router";
-import { ManagerNavBar } from "../../components/manager-nav-bar/manager-nav-bar";
-import { ManagerMenu } from "../../components/manager-menu/manager-menu";
-import { ManagerService } from '../../../../application/manager/services/manager-service';
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { ManagerNavBar } from '../../components/manager-nav-bar/manager-nav-bar';
 
 @Component({
   selector: 'app-manager-side-page',
-  imports: [RouterOutlet, ManagerNavBar, ManagerMenu],
+  imports: [RouterOutlet, ManagerNavBar],
   templateUrl: './manager-side-page.html',
   styleUrl: './manager-side-page.css',
 })
-export class ManagerSidePage {
-
-  managerService = inject(ManagerService);
-  imo = computed(() => {return this.managerService.getIMO})
-  
-
-
-}
+export class ManagerSidePage {}
